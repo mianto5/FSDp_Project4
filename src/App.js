4009fc1a740ec88377ff669f1c4af65935d7076d
mianto5 <tomanovamichaela5@gmail.com>
2023-06-01 17:50:11 +0200

    Creates basic structure of the project.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import AllEvents from './components/AllEvents';
+import Home from './components/Home';
+import Login from './components/Login';
+import MyEvents from './components/MyEvents';
+import MyFavourites from './components/MyFavourites';
+import NavBar from './components/NavBar';
+import Profile from './components/Profile';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/all-events' element={<AllEvents/>}></Route>
+        <Route path='/my-events' element={<MyEvents/>}></Route>
+        <Route path='/my-favourites' element={<MyFavourites/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/MyFavourites.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyFavourites() {
+  return (
+    <div>MyFavourites</div>
+  )
+}
